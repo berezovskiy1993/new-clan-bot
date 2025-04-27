98db4842c77f16de08c2172ee0566cc7bd97da8e
berezovskiy1993 <mksimchik@gmail.com>
2025-04-27 13:02:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ ADMIN_ID = 894031843  # ID администратора
 GROUP_ID = -1002640250280  # ID закрытой группы
 
 # Состояния для ConversationHandler
-NICKNAME, PLAYER_ID, AGE, GENDER, KD_CURRENT, KD_PREVIOUS, MATCHES_CURRENT, MATCHES_PREVIOUS = range(8)
+READY, NICKNAME, PLAYER_ID, AGE, GENDER, KD_CURRENT, KD_PREVIOUS, MATCHES_CURRENT, MATCHES_PREVIOUS = range(9)
 
 # Функция для создания кнопок "Начать с начала" и "Критерии"
 def get_buttons():
@@ -21,10 +21,23 @@ def get_buttons():
 # Стартовая функция
 async def start(update: Update, context: CallbackContext) -> int:
     await update.message.reply_text(
-        "Привет! Я бот клана DEKTRIAN FAMILY. Если хочешь подать заявку на вступление в клан - для начала напиши свой игровой никнейм!",
+        "Привет! Я бот клана DEKTRIAN FAMILY. Если готовы подать заявку на вступление в клан - напишите 'да' или 'нет'.",
         reply_markup=get_buttons()  # Добавляем две кнопки
     )
-    return NICKNAME
+    return READY
+
+# Проверка на готовность подать заявку
+async def ready(update: Update, context: CallbackContext) -> int:
+    user_response = update.message.text.lower()
+    if user_response == 'да':
+        await update.message.reply_text("Напиши свой игровой никнейм.")
+        return NICKNAME
+    elif user_response == 'нет':
+        await update.message.reply_text("Если передумаешь, напиши 'да'.")
+        return READY
+    else:
+        await update.message.reply_text("Пожалуйста, ответь 'да' или 'нет'.")
+        return READY
 
 # Получение никнейма
 async def nickname(update: Update, context: CallbackContext) -> int:
@@ -178,6 +191,7 @@ def main() -> None:
     conversation_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
+            READY: [MessageHandler(filters.TEXT & ~filters.COMMAND, ready)],
             NICKNAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, nickname)],
             PLAYER_ID: [MessageHandler(filters.TEXT & ~filters.COMMAND, player_id)],
             AGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, age)],

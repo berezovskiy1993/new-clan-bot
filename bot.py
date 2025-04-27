253a1a69bc0867a248e4dc190339d62b4a9b485b
berezovskiy1993 <mksimchik@gmail.com>
2025-04-27 14:46:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,13 +21,13 @@ def get_buttons():
 # Стартовая функция
 async def start(update: Update, context: CallbackContext) -> int:
     # Отправляем приветственное сообщение и картинку
-    await update.message.reply_text(
-        "Привет! Я бот клана DEKTRIAN FAMILY. Если готовы подать заявку на вступление в клан - напишите 'да' или 'нет'.",
-        reply_markup=get_buttons()  # Добавляем две кнопки
-    )
     await update.message.reply_photo(
         photo="https://ibb.co/JRbbTWsQ",  # Ссылка на картинку
         caption="Добро пожаловать в DEKTRIAN FAMILY!"  # Подпись под картинкой
+    )
+    await update.message.reply_text(
+        "Привет! Я бот клана DEKTRIAN FAMILY. Если готовы подать заявку на вступление в клан - напишите 'да' или 'нет'.",
+        reply_markup=get_buttons()  # Добавляем две кнопки
     )
     return READY
 

b0a4c57494903479783d968d100e0342df24355e
berezovskiy1993 <mksimchik@gmail.com>
2025-04-28 07:50:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -185,7 +185,8 @@ async def reset(update: Update, context: CallbackContext) -> int:
         "Все данные были сброшены. Начни процесс подачи заявки заново, введя свой игровой никнейм!",
         reply_markup=get_buttons()  # Кнопка сброса
     )
-    return NICKNAME  # Возвращаем пользователя на ввод никнейма
+    # Вернуться в состояние NICKNAME для повторного ввода данных
+    return NICKNAME
 
 # Функция для обработки нажатия на кнопку сброса и критериев
 async def button_callback(update: Update, context: CallbackContext):
